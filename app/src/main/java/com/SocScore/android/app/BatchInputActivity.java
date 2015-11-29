1e0f5c0f372d6a473577e83f18746e4f2d26990b
Lucien-George <lucien.george@mail.mcgill.ca>
2015-11-29 18:10:28 -0500

    added listView to batchInputActivity

--- app/src/main/java/com/SocScore/android/app/BatchInputActivity.java
+++ app/src/main/java/com/SocScore/android/app/BatchInputActivity.java
@@ -1,31 +1,35 @@
 package com.SocScore.android.app;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.SocScore.framework.data.LeagueAnalysis;
 import com.SocScore.framework.data.Team;
 import com.SocScore.framework.scorekeeper.BatchInput;
 
-import org.joda.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BatchInputActivity extends AppCompatActivity {
 
     private FloatingActionButton fab_create_new_team;
     BatchInput batchInput;
-    private EditText et_team1;
-    private EditText et_team2;
     private String str_team1;
     private String str_team2;
     Team team1;
     Team team2;
+    private static List<Team> league = new ArrayList<>();
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,6 +39,21 @@ public class BatchInputActivity extends AppCompatActivity {
         setUpVariables();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.inflateMenu(R.menu.menu_batch);
+        league = LeagueAnalysis.getLeague();
+        ArrayList<String> team_name = new ArrayList<>();
+        for(Team team : league)
+        {
+            team_name.add(team.getName());
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, team_name);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                view.setBackgroundTintList(ColorStateList.valueOf(Color.rgb(210, 140, 56)));
+                view.setSelected(true);
+            }
+        });
         toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
@@ -70,24 +89,50 @@ public class BatchInputActivity extends AppCompatActivity {
         fab_create_new_team.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                str_team1 = et_team1.getText().toString();
-                str_team2 = et_team2.getText().toString();
-                if(LeagueInputActivity.foundTeam(str_team1) && LeagueInputActivity.foundTeam(str_team2))
-                {
-                    team1 = LeagueAnalysis.findTeam(LeagueInputActivity.findTeamID(str_team1));
-                    team2 = LeagueAnalysis.findTeam(LeagueInputActivity.findTeamID(str_team2));
-                    batchInput.createMatch(team1 , team2 , new LocalDateTime() , new LocalDateTime().plusMinutes(90));
-                }
-                else Toast.makeText(getApplicationContext(), "Teams " + str_team1 + " and " + str_team2 + " are not available", Toast.LENGTH_LONG).show();
+//                ArrayList<Team> league_al = (ArrayList)league;
+//                Team [] league_array = league.toArray(new Team[league.size()]);
+//                str_team1 = et_team1.getText().toString();
+//                str_team2 = et_team2.getText().toString();
+//                if(LeagueInputActivity.foundTeam(str_team1) && LeagueInputActivity.foundTeam(str_team2))
+//                {
+//                    team1 = LeagueAnalysis.findTeam(LeagueInputActivity.findTeamID(str_team1));
+//                    team2 = LeagueAnalysis.findTeam(LeagueInputActivity.findTeamID(str_team2));
+//                    batchInput.createMatch(team1 , team2 , new LocalDateTime() , new LocalDateTime().plusMinutes(90));
+//                }
+//                else Toast.makeText(getApplicationContext(), "Teams " + str_team1 + " and " + str_team2 + " are not available", Toast.LENGTH_LONG).show();
             }
         });
     }
 
     public void setUpVariables()
     {
-        et_team1 = (EditText) findViewById(R.id.et_team1);
-        et_team2 = (EditText) findViewById(R.id.et_team2);
+//        et_team1 = (EditText) findViewById(R.id.et_team1);
+//        et_team2 = (EditText) findViewById(R.id.et_team2);
         batchInput = new BatchInput();
+        listView = (ListView) findViewById(R.id.listView);
+    }
+
+    public static int findTeamID(String str)
+    {
+        league = LeagueAnalysis.getLeague();
+        for(Team team : league)
+        {
+            if(team.getName().equalsIgnoreCase(str))
+            {
+                return team.getTEAM_ID();
+            }
+            throw new NullPointerException("Could not find team: " + str);
+        }
+        return -1;
+    }
+
+    public static boolean foundTeam(String str)
+    {
+        if (findTeamID(str) == -1)
+        {
+            return false;
+        }
+        return true;
     }
 
 }
